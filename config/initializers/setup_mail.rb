f60d8afa6a8c279bd812783de4c6683b04c7521e
Kevin Niemi <kevin.niemi@gmail.com>
2013-08-26 13:12:29 -0700

    modify setup_mail.rb to compile on heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,8 @@
 require 'development_mail_interceptor'
 
-ActionMailer::Base.default_url_options[:host] =
-    "localhost:3000" if Rails.env.development?
+ActionMailer::Base.default_url_options[:host] = "localhost:3000" if Rails.env.development?
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
     if Rails.env.development?
 
-ActionMailer::Base.default_url_options[:host] =
-    "shielded-spire-5845.herokuapp.com" if Rails.env.production?
+ActionMailer::Base.default_url_options[:host] = "https://shielded-spire-5845.herokuapp.com" if Rails.env.production?
 
